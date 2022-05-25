e12a1f5d0b5ef4a5b6460a9259d5256b21d5be9b
Karina Ganieva <karinag@lenta.ru>
2022-05-25 20:02:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+db.infostrategic.com user: activitytracking DB: activitytracking password: Fq5AD-zeRd}n32L
---
+++ src/main/java/com/activitytrackhse/ActivitytrackhseApplication.java
@@ -0,0 +1,13 @@
+package com.activitytrackhse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.activitytrackhse"})
+public class ActivitytrackhseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitytrackhseApplication.class, args);
+    }
+
+}
